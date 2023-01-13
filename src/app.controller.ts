03712b133bbc9c87785045200170aaa58bcabe47
Morva <Moravcsikmark@gmail.cim>
2023-01-13 17:23:13 +0100

    kiiratás javitasa(index), tábla kiiratása

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ import {
 import { AppService } from './app.service';
 import db from './db';
 import { CreateCatDto } from './createcat.dto';
+import { get } from 'http';
 
 @Controller()
 export class AppController {
@@ -14,7 +15,15 @@ export class AppController {
 
   @Get()
   @Render('index')
-  async index(@Query('eye_color') szem_szin: string) {
+async index(@Query('szem_szin') szem_szin: string) {
+    if(szem_szin != undefined){
+      const [rows] = await db.execute('SELECT id, suly, szem_szin FROM macskak WHERE szem_szin LIKE ?',['%' + szem_szin + '%'],);
+      return{macskak:rows};
+    }
+    else{
+      const [rows] = await db.execute('SELECT id, szem_szin, suly FROM macskak ORDER BY suly DESC');
+      return{macskak:rows};
+    }
 
   }
 }
