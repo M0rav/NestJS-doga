282457046d03c6b71f1ddf33f8bb2f7c79402d77
Morva <Moravcsikmark@gmail.cim>
2023-01-13 16:32:38 +0100

    macskak kiiratása

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,20 @@
 import {
   Controller,
   Get,
+  Query,
   Render,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
+import { CreateCatDto } from './createcat.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async index(@Query('eye_color') szem_szin: string) {
+
   }
 }
