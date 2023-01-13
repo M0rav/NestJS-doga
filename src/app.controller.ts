f58624de121b0ef93081188251275b575e0ef4b1
Morva <Moravcsikmark@gmail.cim>
2023-01-13 18:37:25 +0100

    macska hozzáadása, kis dizájn

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import {
+  Body,
   Controller,
   Get,
+  Post,
   Query,
+  Redirect,
   Render,
 } from '@nestjs/common';
 import { AppService } from './app.service';
@@ -15,21 +18,31 @@ export class AppController {
 
   @Get()
   @Render('index')
-async index(@Query('szem_szin') szem_szin: string) {
-    if(szem_szin != undefined){
-      const [rows] = await db.execute('SELECT id, suly, szem_szin FROM macskak WHERE szem_szin LIKE ?',['%' + szem_szin + '%'],);
-      return{macskak:rows};
+  async index(@Query('szem_szin') szem_szin: string) {
+    if (szem_szin != undefined) {
+      const [rows] = await db.execute('SELECT id, suly, szem_szin FROM macskak WHERE szem_szin LIKE ?', ['%' + szem_szin + '%'],);
+      return { macskak: rows };
     }
-    else{
+    else {
       const [rows] = await db.execute('SELECT id, szem_szin, suly FROM macskak ORDER BY suly DESC');
-      return{macskak:rows};
+      return { macskak: rows };
     }
-    
+
   }
   @Get('cats/new')
   @Render('addcat')
-  ujcica(){
+  ujcica() {
     return {};
   }
-  
+  @Post('cats/new')
+  @Redirect()
+  async newPainting(@Body() cat: CreateCatDto) {
+    const [result]: any = await db.execute(
+      'INSERT INTO macskak (szem_szin, suly) VALUES (?, ?)',
+      [cat.szem_szin, cat.suly]
+    );
+    return {
+      url: '/',
+    }
+  }
 }
